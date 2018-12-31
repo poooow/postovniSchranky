0c986ac06343c6108fccce083b6e8966a120a550
poooow <poooow@gmail.com>
2018-12-31 15:15:14 +0100

    Set viewport for mobile devices

--- index.php
+++ index.php
@@ -1,39 +1,40 @@
 <!doctype html>
-
 <html lang="cs">
 <head>
-  <meta charset="utf-8">
-  <script type="text/javascript" src="https://api.mapy.cz/loader.js"></script>
-  <script type="text/javascript">Loader.load();</script>
-  <style>
-  body {
-     margin: 0; 
-     padding: 0;  
-  }
-  
-  #info {
-    position: absolute;
-    bottom: 20px;
-    right: 20px;
-    background-color: #00000066;
-    border-radius: 5px;
-    z-index: 99;
-    color: #ffffff;
-    padding: 5px;
-    font-family: Verdana,sans-serif;
-    font-size: 10px;
-  }
-  
-  #info a {
-    color: #ffffff;
-  }
-  
-  #m {
-    position: absolute; 
-    width: 100%; 
-    height: 100%;
-  }
-  </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mapa poštovních schránek v České Republice</title>
+    <script type="text/javascript" src="https://api.mapy.cz/loader.js"></script>
+    <script type="text/javascript">Loader.load();</script>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        #info {
+            position: absolute;
+            bottom: 20px;
+            right: 20px;
+            background-color: #00000066;
+            border-radius: 5px;
+            z-index: 99;
+            color: #ffffff;
+            padding: 5px;
+            font-family: Verdana, sans-serif;
+            font-size: 10px;
+        }
+
+        #info a {
+            color: #ffffff;
+        }
+
+        #m {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
 </head>
 <body>
 <div id="m"></div>
